654abb853a5b00cf7664cc68a8cedb216141d1d1
Robin Wieruch <hello@rwieruch.com>
2024-02-23 08:39:07 +0100

    fix(not-found): finish

--- src/app/tickets/[ticketId]/page.tsx
+++ src/app/tickets/[ticketId]/page.tsx
@@ -1,5 +1,6 @@
 import { TicketItem } from '@/features/ticket/components/ticket-item';
 import { getTicket } from '@/features/ticket/queries/get-ticket';
+import { notFound } from 'next/navigation';
 
 type TicketPageProps = {
   params: {
@@ -11,7 +12,7 @@ const TicketPage = async ({ params }: TicketPageProps) => {
   const ticket = await getTicket(params.ticketId);
 
   if (!ticket) {
-    return <div>Ticket not found</div>;
+    notFound();
   }
 
   return (
