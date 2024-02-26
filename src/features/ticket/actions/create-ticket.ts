4daf299083284bc4e38c8783af0bd37e47728cda
Robin Wieruch <hello@rwieruch.com>
2024-02-26 10:33:23 +0100

    fix(asd): asd

--- src/features/ticket/actions/create-ticket.ts
+++ src/features/ticket/actions/create-ticket.ts
@@ -1,20 +1,39 @@
 'use server';
 
+import { z } from 'zod';
 import { prisma } from '@/lib/prisma';
 import { revalidatePath } from 'next/cache';
 
-export const createTicket = async (formData: FormData) => {
-  const formDataRaw = {
-    title: formData.get('title'),
-    content: formData.get('content'),
-  };
+const createTicketSchema = z.object({
+  title: z.string().min(1).max(191),
+  content: z.string().min(1).max(191),
+});
 
-  await prisma.ticket.create({
-    data: {
-      title: formDataRaw.title as string,
-      content: formDataRaw.content as string,
-    },
-  });
+export const createTicket = async (
+  _formState: { message: string },
+  formData: FormData
+) => {
+  try {
+    const data = createTicketSchema.parse({
+      title: formData.get('title'),
+      content: formData.get('content'),
+    });
+
+    await prisma.ticket.create({
+      data: {
+        title: data.title,
+        content: data.content,
+      },
+    });
+  } catch (error) {
+    return {
+      message: 'Something went wrong',
+    };
+  }
 
   revalidatePath('/tickets');
+
+  return {
+    message: '',
+  };
 };
--- src/features/ticket/components/ticket-create-form.tsx
+++ src/features/ticket/components/ticket-create-form.tsx
@@ -1,17 +1,26 @@
+'use client';
+
+import { useFormState } from 'react-dom';
 import { Label } from '@/components/ui/label';
 import { Input } from '@/components/ui/input';
 import { Textarea } from '@/components/ui/textarea';
-import { SubmitButton } from '@/components/form/submit-button';
 import { createTicket } from '../actions/create-ticket';
+import { SubmitButton } from '@/components/form/submit-button';
 
 const TicketCreateForm = () => {
+  const [formState, action] = useFormState(createTicket, {
+    message: '',
+  });
+
   return (
-    <form action={createTicket} className="flex flex-col gap-y-2">
+    <form action={action} className="flex flex-col gap-y-2">
       <Label htmlFor="title">Title</Label>
       <Input id="title" name="title" />
 
       <Label htmlFor="content">Content</Label>
       <Textarea id="content" name="content" />
+
+      {formState.message}
 
       <SubmitButton />
     </form>
