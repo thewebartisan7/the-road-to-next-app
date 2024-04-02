b030964d47752716b3b2bf303d0bc6a0503de040
Robin Wieruch <hello@rwieruch.com>
2024-04-02 11:09:05 +0200

    fix(asd): asd
    as

--- src/utils/url.ts
+++ src/utils/url.ts
@@ -4,7 +4,7 @@ export const getBaseUrl = () => {
   const baseUrl =
     environment === 'development'
       ? 'http://localhost:3000'
-      : `https://{process.env.NEXT_PUBLIC_VERCEL_URL}`;
+      : `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`;
 
   return baseUrl;
 };
