caa2d3fdb55346727b0ab2a33037d14beec9665d
Robin Wieruch <hello@rwieruch.com>
2024-04-16 13:40:09 +0200

    fix(qwd): qwd

--- src/app/tickets/page.tsx
+++ src/app/tickets/page.tsx
@@ -1,13 +1,14 @@
 import { Suspense } from "react";
 import { Heading } from "@/components/heading";
 import { TicketList } from "@/feature/ticket/components/ticket-list";
+import { Spinner } from "@/components/spinner";
 
 const TicketsPage = () => {
   return (
     <div className="flex-1 flex flex-col gap-y-8">
       <Heading title="Tickets" description="All your tickets at one place" />
 
-      <Suspense>
+      <Suspense fallback={<Spinner />}>
         <TicketList />
       </Suspense>
     </div>
