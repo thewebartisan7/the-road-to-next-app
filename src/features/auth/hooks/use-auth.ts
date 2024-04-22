183767ec42a195419dc135b5a328e5081fcc7db9
Robin Wieruch <hello@rwieruch.com>
2024-04-22 12:15:37 +0200

    fix(qwd): qwd

--- src/features/auth/hooks/use-auth.ts
+++ src/features/auth/hooks/use-auth.ts
@@ -1,10 +1,10 @@
-import { User } from "lucia";
+import { User as AuthUser } from "lucia";
 import { usePathname } from "next/navigation";
 import { useEffect, useState } from "react";
 import { getAuth } from "../queries/get-auth";
 
 const useAuth = () => {
-  const [user, setUser] = useState<User | null>(null);
+  const [user, setUser] = useState<AuthUser | null>(null);
   const [isFetched, setFetched] = useState(false);
 
   const pathname = usePathname();
