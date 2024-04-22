bfb05dd29b00db94399b91d0f79ef7ee9ea398df
Robin Wieruch <hello@rwieruch.com>
2024-04-22 15:08:10 +0200

    fix(qwd): qwd

---
+++ src/features/ticket/components/ticket-pagination.tsx
@@ -0,0 +1,44 @@
+"use client";
+
+import { useQueryState, useQueryStates } from "nuqs";
+import { useEffect, useRef } from "react";
+import { Pagination } from "@/components/pagination";
+import {
+  paginationOptions,
+  paginationParser,
+  searchParser,
+} from "../search-params";
+
+type TicketPaginationProps = {
+  count: number;
+  hasNextPage: boolean;
+};
+
+const TicketPagination = (props: TicketPaginationProps) => {
+  const [pagination, setPagination] = useQueryStates(
+    paginationParser,
+    paginationOptions
+  );
+
+  const [search] = useQueryState("search", searchParser);
+  const prevSearch = useRef(search);
+
+  useEffect(() => {
+    if (search === prevSearch.current) return;
+    prevSearch.current = search;
+
+    setPagination({ ...pagination, page: 0 });
+
+    // add more reactive effects here once needed ...
+  }, [search, pagination, setPagination]);
+
+  return (
+    <Pagination
+      pagination={pagination}
+      onPagination={setPagination}
+      {...props}
+    />
+  );
+};
+
+export { TicketPagination };
