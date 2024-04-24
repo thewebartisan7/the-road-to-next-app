00b91144e539a36cd2c908ee0e0aef60d63da056
Robin Wieruch <hello@rwieruch.com>
2024-04-24 11:25:58 +0200

    fix(qwd): qwd

--- src/features/comment/queries/get-comments.ts
+++ src/features/comment/queries/get-comments.ts
@@ -4,13 +4,13 @@ import { getAuth } from "@/features/auth/queries/get-auth";
 import { isOwner } from "@/features/auth/utils/is-owner";
 import { prisma } from "@/lib/prisma";
 
-export const getComments = async (ticketId: string, cursor?: Date) => {
+export const getComments = async (ticketId: string, cursor?: number) => {
   const { user } = await getAuth();
 
   const where = {
     ticketId,
     createdAt: {
-      lt: cursor,
+      lt: cursor ? new Date(cursor) : undefined,
     },
   };
 
@@ -49,7 +49,7 @@ export const getComments = async (ticketId: string, cursor?: Date) => {
     metadata: {
       count,
       hasNextPage,
-      cursor: comments.at(-1)?.createdAt,
+      cursor: comments.at(-1)?.createdAt.valueOf(),
     },
   };
 };
