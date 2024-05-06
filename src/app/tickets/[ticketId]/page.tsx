83bbdb3ca41cd53345a5cda0ee8915996612738d
Robin Wieruch <hello@rwieruch.com>
2024-05-06 10:41:54 +0200

    fix(2024-v1-link-components2024-v1-link-components): 2024-v1-link-components

--- src/app/tickets/[ticketId]/page.tsx
+++ src/app/tickets/[ticketId]/page.tsx
@@ -1,3 +1,5 @@
+import { initialTickets } from "../../../data";
+
 type TicketPageProps = {
   params: {
     ticketId: string;
@@ -5,7 +7,18 @@ type TicketPageProps = {
 };
 
 const TicketPage = ({ params }: TicketPageProps) => {
-  return <h2 className="text-lg">TicketPage: {params.ticketId}</h2>;
+  const ticket = initialTickets.find((ticket) => ticket.id === params.ticketId);
+
+  if (!ticket) {
+    return <div>Ticket not found</div>;
+  }
+
+  return (
+    <div>
+      <h2 className="text-lg">{ticket.title}</h2>
+      <p className="text-sm">{ticket.content}</p>
+    </div>
+  );
 };
 
 export default TicketPage;
