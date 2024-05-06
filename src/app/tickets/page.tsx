79062e264d1b4c20cf63eedd1789bf20916dfb50
Robin Wieruch <hello@rwieruch.com>
2024-05-06 16:33:37 +0200

    fix(2024-v1-component-folder-heading): 2024-v1-component-folder-heading

--- src/app/tickets/page.tsx
+++ src/app/tickets/page.tsx
@@ -10,6 +10,7 @@ import {
 } from "@/components/ui/card";
 import { Separator } from "@/components/ui/separator";
 import { LucideFileText, LucideCheckCircle, LucidePencil } from "lucide-react";
+import { Heading } from "@/components/heading";
 
 const TICKET_ICONS = {
   OPEN: <LucideFileText />,
@@ -20,14 +21,7 @@ const TICKET_ICONS = {
 const TicketsPage = () => {
   return (
     <div className="flex-1 flex flex-col gap-y-8">
-      <div>
-        <h2 className="text-3xl font-bold tracking-tight">Tickets</h2>
-        <p className="text-sm text-muted-foreground">
-          All your tickets at one place
-        </p>
-      </div>
-
-      <Separator />
+      <Heading title="Tickets" description="All your tickets at one place" />
 
       <div className="flex-1 flex flex-col items-center gap-y-4 animate-fade-in-from-top">
         {initialTickets.map((ticket) => (
