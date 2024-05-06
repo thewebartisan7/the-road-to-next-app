d3d9552adc43f5264c47c63cf11742d5abd1a042
Robin Wieruch <hello@rwieruch.com>
2024-05-06 15:59:15 +0200

    fix(2024-v1-typescript): 2024-v1-typescript
    gits

--- src/app/tickets/page.tsx
+++ src/app/tickets/page.tsx
@@ -3,6 +3,12 @@ import clsx from "clsx";
 import { initialTickets } from "@/data";
 import { ticketPath } from "@/paths";
 
+const TICKET_ICONS = {
+  OPEN: "O",
+  IN_PROGRESS: ">",
+  DONE: "X",
+};
+
 const TicketsPage = () => {
   return (
     <div className="flex-1 flex flex-col gap-y-8">
@@ -19,6 +25,7 @@ const TicketsPage = () => {
             key={ticket.id}
             className="w-full max-w-[420px] p-4 border border-slate-100 rounded"
           >
+            <div>{TICKET_ICONS[ticket.status]}</div>
             <h3 className="text-lg font-semibold truncate">{ticket.title}</h3>
             <p
               className={clsx("text-sm text-slate-500 truncate", {
