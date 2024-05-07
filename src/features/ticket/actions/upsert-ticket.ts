980ff70282f044a55506f35c2e1ca3cc6e92e700
Robin Wieruch <hello@rwieruch.com>
2024-05-07 15:54:55 +0200

    fix(2024-v1-form-validation): 2024-v1-form-validation

--- src/features/ticket/actions/upsert-ticket.ts
+++ src/features/ticket/actions/upsert-ticket.ts
@@ -2,29 +2,30 @@
 
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
+import { z } from "zod";
 import { prisma } from "@/lib/prisma";
 import { ticketPath, ticketsPath } from "@/paths";
+
+const upsertTicketSchema = z.object({
+  title: z.string().min(1).max(191),
+  content: z.string().min(1).max(1024),
+});
 
 export const upsertTicket = async (
   id: string | undefined,
   formData: FormData
 ) => {
-  const data = {
+  const data = upsertTicketSchema.parse({
     title: formData.get("title"),
     content: formData.get("content"),
-  };
-
-  const dbData = {
-    title: data.title as string,
-    content: data.content as string,
-  };
+  });
 
   await prisma.ticket.upsert({
     where: {
       id: id || "",
     },
-    update: dbData,
-    create: dbData,
+    update: data,
+    create: data,
   });
 
   revalidatePath(ticketsPath());
