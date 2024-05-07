31c37810fe17f4b91fe4d531a492ea928da1def8
Robin Wieruch <hello@rwieruch.com>
2024-05-07 16:19:43 +0200

    fix(2024-v1-error-handling): 2024-v1-error-handling

--- src/features/ticket/actions/upsert-ticket.ts
+++ src/features/ticket/actions/upsert-ticket.ts
@@ -3,6 +3,10 @@
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
 import { z } from "zod";
+import {
+  ActionState,
+  fromErrorToActionState,
+} from "@/components/form/utils/to-action-state";
 import { prisma } from "@/lib/prisma";
 import { ticketPath, ticketsPath } from "@/paths";
 
@@ -13,7 +17,7 @@ const upsertTicketSchema = z.object({
 
 export const upsertTicket = async (
   id: string | undefined,
-  _formState: { message: string },
+  _formState: ActionState,
   formData: FormData
 ) => {
   try {
@@ -30,7 +34,7 @@ export const upsertTicket = async (
       create: data,
     });
   } catch (error) {
-    return { message: "Something went wrong" };
+    return fromErrorToActionState(error);
   }
 
   revalidatePath(ticketsPath());
