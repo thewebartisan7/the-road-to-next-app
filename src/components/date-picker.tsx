90c5960237efd61e26e8a64dca71ac00956392d6
Robin Wieruch <hello@rwieruch.com>
2024-05-08 11:52:30 +0200

    fix(2024-v1-close-date-picker): 2024-v1-close-date-picker

--- src/components/date-picker.tsx
+++ src/components/date-picker.tsx
@@ -18,6 +18,13 @@ type DatePickerProps = {
 };
 
 const DatePicker = ({ id, name, defaultValue }: DatePickerProps) => {
+  const [open, setOpen] = useState(false);
+
+  const handleSelect = (selectedDate: Date | undefined) => {
+    setDate(selectedDate);
+    setOpen(false);
+  };
+
   const [date, setDate] = useState<Date | undefined>(
     defaultValue ? new Date(defaultValue) : new Date()
   );
@@ -25,7 +32,7 @@ const DatePicker = ({ id, name, defaultValue }: DatePickerProps) => {
   const formattedStringDate = date ? format(date, "yyyy-MM-dd") : "";
 
   return (
-    <Popover>
+    <Popover open={open} onOpenChange={setOpen}>
       <PopoverTrigger id={id} className="w-full" asChild>
         <Button
           variant="outline"
@@ -40,7 +47,7 @@ const DatePicker = ({ id, name, defaultValue }: DatePickerProps) => {
         <Calendar
           mode="single"
           selected={date}
-          onSelect={setDate}
+          onSelect={handleSelect}
           initialFocus
         />
       </PopoverContent>
