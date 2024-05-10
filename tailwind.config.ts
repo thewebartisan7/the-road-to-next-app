c04663ac005c4eec89fdda529d06c77d8163943f
Robin Wieruch <hello@rwieruch.com>
2024-05-10 14:55:29 +0200

    fix(2024-v1-authenticated-sidebar2024-v1-authenticated-sidebar): 2024-v1-authenticated-sidebar

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -75,12 +75,17 @@ const config = {
           from: { transform: "translateY(-100%)" },
           to: { transform: "translateY(0)" },
         },
+        "sidebar-from-left": {
+          from: { transform: "translateX(-100%)" },
+          to: { transform: "translateX(0)" },
+        },
       },
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
         "fade-in-from-top": "fade-in-from-top 0.5s ease-out",
         "header-from-top": "header-from-top 0.5s ease-out",
+        "sidebar-from-left": "sidebar-from-left 0.5s ease-out",
       },
     },
   },
