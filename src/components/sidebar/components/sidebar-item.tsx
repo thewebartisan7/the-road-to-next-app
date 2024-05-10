475ebfc201f87e17a17955717abaa3c2c2afa9cc
Robin Wieruch <hello@rwieruch.com>
2024-05-10 15:22:26 +0200

    fix(2024-v1-active-path): 2024-v1-active-path

--- src/components/sidebar/components/sidebar-item.tsx
+++ src/components/sidebar/components/sidebar-item.tsx
@@ -1,5 +1,4 @@
 import Link from "next/link";
-import { usePathname } from "next/navigation";
 import { cloneElement } from "react";
 import { buttonVariants } from "@/components/ui/button";
 import { Separator } from "@/components/ui/separator";
@@ -9,13 +8,11 @@ import { NavItem } from "../types";
 
 type SidebarItemProps = {
   isOpen: boolean;
+  isActive: boolean;
   navItem: NavItem;
 };
 
-const SidebarItem = ({ isOpen, navItem }: SidebarItemProps) => {
-  const path = usePathname();
-  const isActive = path === navItem.href;
-
+const SidebarItem = ({ isOpen, isActive, navItem }: SidebarItemProps) => {
   return (
     <>
       {navItem.separator && <Separator />}
