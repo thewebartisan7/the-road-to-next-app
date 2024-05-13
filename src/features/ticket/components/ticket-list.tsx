706cc673b738697bdff63cfb8e2e7af8e5554c2e
Robin Wieruch <hello@rwieruch.com>
2024-05-13 11:24:46 +0200

    fix(2024-v1-sort): 2024-v1-sort

--- src/features/ticket/components/ticket-list.tsx
+++ src/features/ticket/components/ticket-list.tsx
@@ -1,5 +1,6 @@
 import { Placeholder } from "@/components/placeholder";
 import { SearchInput } from "@/components/search-input";
+import { SortSelect } from "@/components/sort-select";
 import { getTickets } from "../queries/get-tickets";
 import { SearchParams } from "../search-params";
 import { TicketItem } from "./ticket-item";
@@ -14,8 +15,15 @@ const TicketList = async ({ userId, searchParams }: TicketListProps) => {
 
   return (
     <div className="flex-1 flex flex-col items-center gap-y-4 animate-fade-in-from-top">
-      <div className="w-full max-w-[420px]">
+      <div className="w-full max-w-[420px] flex gap-x-2">
         <SearchInput placeholder="Search tickets ..." />
+        <SortSelect
+          defaulltValue="newest"
+          options={[
+            { value: "newest", label: "Newest" },
+            { value: "bounty", label: "Bounty" },
+          ]}
+        />
       </div>
 
       {tickets.length ? (
