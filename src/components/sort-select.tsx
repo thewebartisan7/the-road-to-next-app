1fc2bc62569cf02a24d298a3d318fc9c9c483b6f
Robin Wieruch <hello@rwieruch.com>
2024-05-13 11:42:07 +0200

    fix(2024-v1-usequerystate): 2024-v1-usequerystate

--- src/components/sort-select.tsx
+++ src/components/sort-select.tsx
@@ -1,6 +1,7 @@
 "use client";
 
-import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import { useQueryState } from "nuqs";
+import { sortParser } from "@/features/ticket/search-params";
 import {
   Select,
   SelectContent,
@@ -15,36 +16,18 @@ type Option = {
 };
 
 type SortSelectProps = {
-  defaulltValue?: string;
   options: Option[];
 };
 
-const SortSelect = ({ defaulltValue, options }: SortSelectProps) => {
-  const searchParams = useSearchParams();
-  const pathname = usePathname();
-  const { replace } = useRouter();
+const SortSelect = ({ options }: SortSelectProps) => {
+  const [sort, setSort] = useQueryState("sort", sortParser);
 
   const handleSort = (value: string) => {
-    const params = new URLSearchParams(searchParams);
-
-    if (value === defaulltValue) {
-      params.delete("sort");
-    } else if (value) {
-      params.set("sort", value);
-    } else {
-      params.delete("sort");
-    }
-
-    replace(`${pathname}?${params.toString()}`, {
-      scroll: false,
-    });
+    setSort(value);
   };
 
   return (
-    <Select
-      onValueChange={handleSort}
-      defaultValue={searchParams.get("sort")?.toString() || defaulltValue}
-    >
+    <Select onValueChange={handleSort} defaultValue={sort}>
       <SelectTrigger>
         <SelectValue />
       </SelectTrigger>
