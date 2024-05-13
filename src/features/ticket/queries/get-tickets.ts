f83765611b97774689b80a2d52192ef94cc00a22
Robin Wieruch <hello@rwieruch.com>
2024-05-13 11:10:01 +0200

    fix(2024-v1-searchparams): 2024-v1-searchparams

--- src/features/ticket/queries/get-tickets.ts
+++ src/features/ticket/queries/get-tickets.ts
@@ -1,9 +1,16 @@
 import { prisma } from "@/lib/prisma";
+import { SearchParams } from "../search-params";
 
-export const getTickets = async (userId: string | undefined) => {
+export const getTickets = async (
+  userId: string | undefined,
+  searchParams: SearchParams
+) => {
   return await prisma.ticket.findMany({
     where: {
       userId,
+      title: {
+        contains: searchParams.search,
+      },
     },
     orderBy: {
       createdAt: "desc",
