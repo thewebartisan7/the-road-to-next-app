a8f06cd2628004ef30299d06a29d9b10d50d93a7
Robin Wieruch <hello@rwieruch.com>
2024-05-13 17:32:58 +0200

    fix(2024-v1-react-query-provider): 2024-v1-react-query-provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import { Sidebar } from "@/app/_navigation/sidebar/components/sidebar";
 import { ThemeProvider } from "@/components/theme/theme-provider";
 import { Toaster } from "@/components/ui/sonner";
 import { Header } from "./_navigation/header";
+import { ReactQueryProvider } from "./_providers/react-query/react-query-provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -22,22 +23,24 @@ export default function RootLayout({
     <html suppressHydrationWarning lang="en">
       <body className={inter.className}>
         <ThemeProvider>
-          <Header />
-          <div className="flex h-screen overflow-hidden border-collapse">
-            <Sidebar />
-            <main
-              className="
+          <ReactQueryProvider>
+            <Header />
+            <div className="flex h-screen overflow-hidden border-collapse">
+              <Sidebar />
+              <main
+                className="
                 min-h-screen flex-1
                 overflow-y-auto overflow-x-hidden
                 py-24 px-8
                 bg-secondary/20
                 flex flex-col
               "
-            >
-              {children}
-            </main>
-          </div>
-          <Toaster expand />
+              >
+                {children}
+              </main>
+            </div>
+            <Toaster expand />
+          </ReactQueryProvider>
         </ThemeProvider>
       </body>
     </html>
