7728b8f076d06b7cc9f49ebcb55d21969239c44a
Robin Wieruch <hello@rwieruch.com>
2024-05-13 16:19:10 +0200

    fix(2024-v1-typescript-generics): 2024-v1-typescript-generics

--- src/components/form/utils/to-action-state.ts
+++ src/components/form/utils/to-action-state.ts
@@ -1,11 +1,11 @@
 import { ZodError } from "zod";
 
-export type ActionState = {
+export type ActionState<T = any> = {
   status: "IDLE" | "SUCCESS" | "ERROR";
   message: string;
   fieldErrors: Record<string, string[] | undefined>;
   timestamp: number;
-  data?: unknown;
+  data?: T;
 };
 
 export const EMPTY_ACTION_STATE: ActionState = {
