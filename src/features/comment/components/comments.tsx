ae3f9cdc6980f526eba572d518508319424392e5
Robin Wieruch <hello@rwieruch.com>
2024-05-13 15:52:00 +0200

    fix(2024-v1-use-server): 2024-v1-use-server

--- src/features/comment/components/comments.tsx
+++ src/features/comment/components/comments.tsx
@@ -2,6 +2,7 @@
 
 import { CardCompact } from "@/components/card-compact";
 import { Button } from "@/components/ui/button";
+import { getComments } from "../queries/get-comments";
 import { CommentWithMetadata } from "../types";
 import { CommentCreateForm } from "./comment-create-form";
 import { CommentDeleteButton } from "./comment-delete-button";
@@ -13,6 +14,11 @@ type CommentsProps = {
 };
 
 const Comments = ({ ticketId, comments }: CommentsProps) => {
+  const handleMore = async () => {
+    const result = await getComments(ticketId);
+    console.log(result);
+  };
+
   return (
     <>
       <CardCompact
@@ -36,7 +42,7 @@ const Comments = ({ ticketId, comments }: CommentsProps) => {
       </div>
 
       <div className="flex flex-col justify-center ml-8">
-        <Button variant="ghost" onClick={() => {}}>
+        <Button variant="ghost" onClick={handleMore}>
           More
         </Button>
       </div>
