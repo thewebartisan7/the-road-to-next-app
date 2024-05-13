f9de302db3c02068a27ab2e66bd8e4a86b4251ca
Robin Wieruch <hello@rwieruch.com>
2024-05-13 10:40:16 +0200

    fix(2024-v1-private-folders): 2024-v1-private-folders

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,10 @@
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import { Header } from "@/components/header";
-import { Sidebar } from "@/components/sidebar/components/sidebar";
+import { Sidebar } from "@/app/_navigation/sidebar/components/sidebar";
 import { ThemeProvider } from "@/components/theme/theme-provider";
 import { Toaster } from "@/components/ui/sonner";
+import { Header } from "./_navigation/header";
 
 const inter = Inter({ subsets: ["latin"] });
 
