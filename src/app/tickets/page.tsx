ed0dc3df45da79410bc7b32ecd4c3151b8c1c153
Robin Wieruch <hello@rwieruch.com>
2024-05-14 16:05:48 +0200

    fix(2024-v1-error-boundary): 2024-v1-error-boundary

--- src/app/tickets/page.tsx
+++ src/app/tickets/page.tsx
@@ -1,5 +1,7 @@
 import { Suspense } from "react";
+import { ErrorBoundary } from "react-error-boundary";
 import { Heading } from "@/components/heading";
+import { Placeholder } from "@/components/placeholder";
 import { Spinner } from "@/components/spinner";
 import { TicketList } from "@/features/ticket/components/ticket-list";
 
@@ -9,7 +11,9 @@ const TicketsPage = async () => {
       <Heading title="Tickets" description="All your tickets at one place" />
 
       <Suspense fallback={<Spinner />}>
-        <TicketList />
+        <ErrorBoundary fallback={<Placeholder label="Something went wrong!" />}>
+          <TicketList />
+        </ErrorBoundary>
       </Suspense>
     </div>
   );
