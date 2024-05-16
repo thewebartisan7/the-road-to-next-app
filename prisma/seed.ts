9e9f1e57c0042ffa1b81bc405b3923513b9ca70f
Robin Wieruch <hello@rwieruch.com>
2024-05-16 14:34:35 +0200

    fix(2024-v1-many-to-one-relation): 2024-v1-many-to-one-relation

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,6 +1,19 @@
+import { hash } from "@node-rs/argon2";
 import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
+
+const users = [
+  {
+    username: "admin",
+    email: "admin@admin.com",
+  },
+  {
+    username: "user",
+    // use your own email here
+    email: "daniel.drehmann@gmail.com",
+  },
+];
 
 const tickets = [
   {
@@ -31,9 +44,22 @@ const seed = async () => {
   console.log("DB Seed: Started ...");
 
   await prisma.ticket.deleteMany();
+  await prisma.user.deleteMany();
+
+  const passwordHash = await hash("password");
+
+  const dbUsers = await prisma.user.createManyAndReturn({
+    data: users.map((user) => ({
+      ...user,
+      passwordHash,
+    })),
+  });
 
   await prisma.ticket.createMany({
-    data: tickets,
+    data: tickets.map((ticket) => ({
+      ...ticket,
+      userId: dbUsers[0].id,
+    })),
   });
 
   const t1 = performance.now();
