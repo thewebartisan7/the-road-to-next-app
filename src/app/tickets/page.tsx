ec36c169421c8e18e6e8ed8a5ece5f2afa9220c2
Robin Wieruch <hello@rwieruch.com>
2024-10-14 16:17:59 +0200

    fix(2024-10_files-based-routing_path-constants): 2024-10_files-based-routing_path-constants

--- src/app/tickets/page.tsx
+++ src/app/tickets/page.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import { initialTickets } from "@/data";
+import { ticketPath } from "@/paths";
 
 const TicketsPage = () => {
   return (
@@ -8,7 +9,7 @@ const TicketsPage = () => {
         <div key={ticket.id}>
           <h2 className="text-lg">{ticket.title}</h2>
 
-          <Link href={`/tickets/${ticket.id}`} className="text-sm underline">
+          <Link href={ticketPath(ticket.id)} className="text-sm underline">
             View
           </Link>
         </div>
