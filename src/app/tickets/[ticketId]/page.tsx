1c803e0be60200e94aaf04779cdd73d5dd36d55e
Robin Wieruch <hello@rwieruch.com>
2024-11-05 17:25:28 +0100

    fix(2024-11_files-based-routing_link-components): 2024-11_files-based-routing_link-components

--- src/app/tickets/[ticketId]/page.tsx
+++ src/app/tickets/[ticketId]/page.tsx
@@ -1,3 +1,5 @@
+import { initialTickets } from "../../../data";
+
 type TicketPageProps = {
   params: Promise<{
     ticketId: string;
@@ -7,7 +9,18 @@ type TicketPageProps = {
 const TicketPage = async ({ params }: TicketPageProps) => {
   const { ticketId } = await params;
 
-  return <h2 className="text-lg">TicketPage: {ticketId}</h2>;
+  const ticket = initialTickets.find((ticket) => ticket.id === ticketId);
+
+  if (!ticket) {
+    return <div>Ticket not found</div>;
+  }
+
+  return (
+    <div>
+      <h2 className="text-lg">{ticket.title}</h2>
+      <p className="text-sm">{ticket.content}</p>
+    </div>
+  );
 };
 
 export default TicketPage;
