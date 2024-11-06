c6a7150484de228893a41f780302830d9b81800c
Robin Wieruch <hello@rwieruch.com>
2024-11-06 16:46:31 +0100

    fix(2024-11_server-components_suspense): 2024-11_server-components_suspense

--- src/app/tickets/page.tsx
+++ src/app/tickets/page.tsx
@@ -1,5 +1,6 @@
 import { Suspense } from "react";
 import { Heading } from "@/components/heading";
+import { Spinner } from "@/components/spinner";
 import { TicketList } from "@/features/ticket/components/ticket-list";
 
 const TicketsPage = async () => {
@@ -7,7 +8,7 @@ const TicketsPage = async () => {
     <div className="flex-1 flex flex-col gap-y-8">
       <Heading title="Tickets" description="All your tickets at one place" />
 
-      <Suspense>
+      <Suspense fallback={<Spinner />}>
         <TicketList />
       </Suspense>
     </div>
