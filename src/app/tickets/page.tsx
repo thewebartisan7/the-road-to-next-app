afc1276a17908f16436c037f0db27cc750b226a3
Robin Wieruch <hello@rwieruch.com>
2024-11-06 16:44:53 +0100

    fix(2024-11_server-components_streaming): 2024-11_server-components_streaming

--- src/app/tickets/page.tsx
+++ src/app/tickets/page.tsx
@@ -1,19 +1,15 @@
+import { Suspense } from "react";
 import { Heading } from "@/components/heading";
-import { TicketItem } from "@/features/ticket/components/ticket-item";
-import { getTickets } from "@/features/ticket/queries/get-tickets";
+import { TicketList } from "@/features/ticket/components/ticket-list";
 
 const TicketsPage = async () => {
-  const tickets = await getTickets();
-
   return (
     <div className="flex-1 flex flex-col gap-y-8">
       <Heading title="Tickets" description="All your tickets at one place" />
 
-      <div className="flex-1 flex flex-col items-center gap-y-4 animate-fade-from-top">
-        {tickets.map((ticket) => (
-          <TicketItem key={ticket.id} ticket={ticket} />
-        ))}
-      </div>
+      <Suspense>
+        <TicketList />
+      </Suspense>
     </div>
   );
 };
