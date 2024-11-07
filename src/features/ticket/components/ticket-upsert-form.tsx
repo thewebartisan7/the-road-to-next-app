7f5775797d672479724776e5823eb632de2aaf40
Robin Wieruch <hello@rwieruch.com>
2024-11-07 17:32:24 +0100

    fix(2024-11_database-migrations_dates): 2024-11_database-migrations_dates

--- src/features/ticket/components/ticket-upsert-form.tsx
+++ src/features/ticket/components/ticket-upsert-form.tsx
@@ -2,6 +2,7 @@
 
 import { Ticket } from "@prisma/client";
 import { useActionState } from "react";
+import { DatePicker } from "@/components/date-picker";
 import { FieldError } from "@/components/form/field-error";
 import { Form } from "@/components/form/form";
 import { SubmitButton } from "@/components/form/submit-button";
@@ -48,10 +49,9 @@ const TicketUpsertForm = ({ ticket }: TicketUpsertFormProps) => {
       <div className="flex gap-x-2 mb-1">
         <div className="w-1/2">
           <Label htmlFor="deadline">Deadline</Label>
-          <Input
+          <DatePicker
             id="deadline"
             name="deadline"
-            type="date"
             defaultValue={
               (actionState.payload?.get("deadline") as string) ??
               ticket?.deadline
