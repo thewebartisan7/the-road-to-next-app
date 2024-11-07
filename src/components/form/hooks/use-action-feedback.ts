2bea8d6d8897c57a489e8258e7484603c79b6950
Robin Wieruch <hello@rwieruch.com>
2024-11-07 15:38:52 +0100

    fix(2024-11_forms_action-debugging): 2024-11_forms_action-debugging

--- src/components/form/hooks/use-action-feedback.ts
+++ src/components/form/hooks/use-action-feedback.ts
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 import { ActionState } from "@/components/form/utils/to-action-state";
 
 type OnArgs = {
@@ -14,7 +14,12 @@ const useActionFeedback = (
   actionState: ActionState,
   options: UseActionFeedbackOptions
 ) => {
+  const prevTimestamp = useRef(actionState.timestamp);
+  const isUpdate = prevTimestamp.current !== actionState.timestamp;
+
   useEffect(() => {
+    if (!isUpdate) return;
+
     if (actionState.status === "SUCCESS") {
       options.onSuccess?.({ actionState });
     }
@@ -22,7 +27,9 @@ const useActionFeedback = (
     if (actionState.status === "ERROR") {
       options.onError?.({ actionState });
     }
-  }, [actionState, options]);
+
+    prevTimestamp.current = actionState.timestamp;
+  }, [isUpdate, actionState, options]);
 };
 
 export { useActionFeedback };
