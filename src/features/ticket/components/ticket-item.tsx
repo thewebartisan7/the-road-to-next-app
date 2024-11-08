76cb9b54315554293e24cb6deaffed123ab609f5
Robin Wieruch <hello@rwieruch.com>
2024-11-08 13:04:15 +0100

    fix(2024-11_advanced-ui_confirm-dialog): 2024-11_advanced-ui_confirm-dialog

--- src/features/ticket/components/ticket-item.tsx
+++ src/features/ticket/components/ticket-item.tsx
@@ -7,6 +7,7 @@ import {
   LucideTrash,
 } from "lucide-react";
 import Link from "next/link";
+import { ConfirmDialog } from "@/components/confirm-dialog";
 import { Button } from "@/components/ui/button";
 import {
   Card,
@@ -44,11 +45,14 @@ const TicketItem = ({ ticket, isDetail }: TicketItemProps) => {
   );
 
   const deleteButton = (
-    <form action={deleteTicket.bind(null, ticket.id)}>
-      <Button variant="outline" size="icon">
-        <LucideTrash className="h-4 w-4" />
-      </Button>
-    </form>
+    <ConfirmDialog
+      action={deleteTicket.bind(null, ticket.id)}
+      trigger={
+        <Button variant="outline" size="icon">
+          <LucideTrash className="h-4 w-4" />
+        </Button>
+      }
+    />
   );
 
   const moreMenu = (
