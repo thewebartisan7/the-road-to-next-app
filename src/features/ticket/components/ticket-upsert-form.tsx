1643f83461043d51bd32a16d2ee3c42ed706e238
Robin Wieruch <hello@rwieruch.com>
2024-11-08 11:54:00 +0100

    fix(2024-11_advanced-ui_exposing-callback-handlers): 2024-11_advanced-ui_exposing-callback-handlers

--- src/features/ticket/components/ticket-upsert-form.tsx
+++ src/features/ticket/components/ticket-upsert-form.tsx
@@ -23,8 +23,12 @@ const TicketUpsertForm = ({ ticket }: TicketUpsertFormProps) => {
     EMPTY_ACTION_STATE
   );
 
+  const handleSuccess = () => {
+    console.log("Success Handler");
+  };
+
   return (
-    <Form action={action} actionState={actionState}>
+    <Form action={action} actionState={actionState} onSuccess={handleSuccess}>
       <Label htmlFor="title">Title</Label>
       <Input
         id="title"
