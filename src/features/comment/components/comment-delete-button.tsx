4c9eae549c300d6aa2d9e582eb04b10a93b3e330
Robin Wieruch <hello@rwieruch.com>
2024-11-11 12:51:31 +0100

    fix(2024-11_client-side-react_remove-comment): 2024-11_client-side-react_remove-comment

--- src/features/comment/components/comment-delete-button.tsx
+++ src/features/comment/components/comment-delete-button.tsx
@@ -7,9 +7,13 @@ import { deleteComment } from "../actions/delete-comment";
 
 type CommentDeleteButtonProps = {
   id: string;
+  onDeleteComment?: (id: string) => void;
 };
 
-const CommentDeleteButton = ({ id }: CommentDeleteButtonProps) => {
+const CommentDeleteButton = ({
+  id,
+  onDeleteComment,
+}: CommentDeleteButtonProps) => {
   const [deleteButton, deleteDialog] = useConfirmDialog({
     action: deleteComment.bind(null, id),
     trigger: (
@@ -17,6 +21,7 @@ const CommentDeleteButton = ({ id }: CommentDeleteButtonProps) => {
         <LucideTrash className="w-4 h-4" />
       </Button>
     ),
+    onSuccess: () => onDeleteComment?.(id),
   });
 
   return (
