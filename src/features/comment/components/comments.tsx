98a457ff8bc16dccf4fb934c90c966a68451cef0
Robin Wieruch <hello@rwieruch.com>
2024-11-11 13:36:41 +0100

    fix(2024-11_cursor-based-pagination_timestamp): 2024-11_cursor-based-pagination_timestamp

--- src/features/comment/components/comments.tsx
+++ src/features/comment/components/comments.tsx
@@ -13,7 +13,7 @@ type CommentsProps = {
   ticketId: string;
   paginatedComments: {
     list: CommentWithMetadata[];
-    metadata: { count: number; hasNextPage: boolean };
+    metadata: { count: number; hasNextPage: boolean; cursor?: number };
   };
 };
 
@@ -22,7 +22,7 @@ const Comments = ({ ticketId, paginatedComments }: CommentsProps) => {
   const [metadata, setMetadata] = useState(paginatedComments.metadata);
 
   const handleMore = async () => {
-    const morePaginatedComments = await getComments(ticketId, comments.length);
+    const morePaginatedComments = await getComments(ticketId, metadata.cursor);
     const moreComments = morePaginatedComments.list;
 
     setComments([...comments, ...moreComments]);
