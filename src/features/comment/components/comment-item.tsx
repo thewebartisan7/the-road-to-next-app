5cc6a1578981d2660631716f17d42008b49c90ee
Robin Wieruch <hello@rwieruch.com>
2024-11-11 12:13:09 +0100

    fix(2024-11_client-components_implicit): 2024-11_client-components_implicit

--- src/features/comment/components/comment-item.tsx
+++ src/features/comment/components/comment-item.tsx
@@ -12,7 +12,7 @@ const CommentItem = ({ comment, buttons }: CommentItemProps) => {
       <Card className="p-4 flex-1 flex flex-col gap-y-1">
         <div className="flex justify-between">
           <p className="text-sm text-muted-foreground">
-            {comment.user?.username ?? "Deleted User"}
+            {comment.isOwner ? "You" : comment.user?.username ?? "Deleted User"}
           </p>
           <p className="text-sm text-muted-foreground">
             {comment.createdAt.toLocaleString()}
