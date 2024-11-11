9103d9b3aefe96f6e152ebb4ce9d415a88b29bb3
Robin Wieruch <hello@rwieruch.com>
2024-11-11 11:47:32 +0100

    fix(2024-11_data-fetching-patterns): 2024-11_data-fetching-patterns

--- src/features/comment/queries/get-comments.ts
+++ src/features/comment/queries/get-comments.ts
@@ -1,6 +1,9 @@
 import { prisma } from "@/lib/prisma";
 
 export const getComments = async (ticketId: string) => {
+  console.log("comments");
+  await new Promise((resolve) => setTimeout(resolve, 2000));
+
   return await prisma.comment.findMany({
     where: {
       ticketId,
