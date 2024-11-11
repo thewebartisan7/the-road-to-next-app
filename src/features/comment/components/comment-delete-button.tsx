9c6f128ae378c1d6c0f234e34e289bae6c5a08a4
Robin Wieruch <hello@rwieruch.com>
2024-11-11 13:29:49 +0100

    fix(2024-11_unblocking-ui_delete-feedback-button): 2024-11_unblocking-ui_delete-feedback-button

--- src/features/comment/components/comment-delete-button.tsx
+++ src/features/comment/components/comment-delete-button.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { LucideTrash } from "lucide-react";
+import { LucideLoaderCircle, LucideTrash } from "lucide-react";
 import { useConfirmDialog } from "@/components/confirm-dialog";
 import { Button } from "@/components/ui/button";
 import { deleteComment } from "../actions/delete-comment";
@@ -16,9 +16,13 @@ const CommentDeleteButton = ({
 }: CommentDeleteButtonProps) => {
   const [deleteButton, deleteDialog] = useConfirmDialog({
     action: deleteComment.bind(null, id),
-    trigger: (
+    trigger: (isPending) => (
       <Button variant="outline" size="icon">
-        <LucideTrash className="w-4 h-4" />
+        {isPending ? (
+          <LucideLoaderCircle className="w-4 h-4 animate-spin" />
+        ) : (
+          <LucideTrash className="w-4 h-4" />
+        )}
       </Button>
     ),
     onSuccess: () => onDeleteComment?.(id),
